964ae3066910411f0e0aab65050930a5c6127151
kooldrmony <derrickreyes.23@gmail.com>
2018-09-19 20:56:14 -0700

    initial setup of importing redux and react-redux

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './src/reducers';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View style={styles.container}>
+          <Text>Hola Bitches! Is this working?</Text>
+        </View>
+      </Provider>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
