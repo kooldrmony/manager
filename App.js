e360a87019055f171bb702acc2b6075bf0667f0f
kooldrmony <derrickreyes.23@gmail.com>
2018-09-19 21:09:46 -0700

    added the firebase library and added componentWillMount method with firebase authentication code

--- App.js
+++ App.js
@@ -2,9 +2,22 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './src/reducers';
 
 export default class App extends React.Component {
+  componentWillMount() {
+    const config = {
+    apiKey: "AIzaSyD2DxJDopUU_-Rf6Rb6QRVF8p56kJyjH8o",
+    authDomain: "manager-8d634.firebaseapp.com",
+    databaseURL: "https://manager-8d634.firebaseio.com",
+    projectId: "manager-8d634",
+    storageBucket: "manager-8d634.appspot.com",
+    messagingSenderId: "367606535778"
+  };
+  firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
