9de8646965f133c871e2ed47cf21c066c5cc6cdb
kooldrmony <derrickreyes.23@gmail.com>
2018-09-20 07:35:05 -0700

    added common folder boiler plate components

---
+++ src/components/LoginForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Card } from './common';
+
+export LoginForm extends Component {
+	render() {
+		return (
+			<Card>
+			</Card>
+			)
+	}
+}
+
+export default LoginForm;
