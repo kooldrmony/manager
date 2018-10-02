3bf3e35db323a40edd6a1fc3133397bd7c34da05
kooldrmony <derrickreyes.23@gmail.com>
2018-10-02 16:53:40 -0700

    added the AuthAction from and setup index.js in actions

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Router, Scene } from 'react-native-router-flux';
+import { Router, Scene, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
 
 const RouterComponent = () => {
 	return (
@@ -13,12 +14,15 @@ const RouterComponent = () => {
 				<Scene key="main">
 					<Scene 
 					rightTitle="Add"
-					onRight={() => { console.log('right!!!') }}
+					onRight={() => Actions.employeeCreate()}
 					key="employeeList" 
 					component={EmployeeList} 
 					title="Employees" 
-				/>
+					initial
+					/>
 				</Scene>
+				<Scene key="employeeCreate" component={EmployeeCreate} title="Create Employee" />
+
 			</Scene>
 		</Router>
 
