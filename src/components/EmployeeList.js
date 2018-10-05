1c7343295e1ccc073532d66a6cca07fcbaef2f20
kooldrmony <derrickreyes.23@gmail.com>
2018-10-05 07:50:59 -0700

    added employeereducer file

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { employeesFetch } from '../actions';
 import { View, Text } from 'react-native';
 
 class EmployeeList extends Component {
+	componentWillMount() {
+		this.props.employeesFetch;
+	}
+	
 	render() {
 		return (
 			<View>
@@ -15,4 +21,4 @@ class EmployeeList extends Component {
 	}
 };
 
-export default EmployeeList;
+export default connect(null, { employeesFetch })(EmployeeList);
