584a850448958059e0624b85495b6ca3e324f0cf
kooldrmony <derrickreyes.23@gmail.com>
2018-10-07 19:28:25 -0700

    added employee form and employee edit components

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { employeeUpdate, employeeCreate } from '../actions';
 import { View, Text } from 'react-native';
 import { Card, CardSection, Input, Button } from './common';
+import EmployeeForm from './EmployeeForm';
 
 class EmployeeCreate extends Component {
 	onButtonPress() {
@@ -17,41 +18,7 @@ class EmployeeCreate extends Component {
 	render() {
 		return(
 			<Card>
-				<CardSection>
-					<Input
-						label="Name"
-						placeholder="Jane"
-						value={this.props.name}
-						onChangeText={value => this.props.employeeUpdate({ prop: 'name', value })}
-					/>
-				</CardSection>
-
-				<CardSection>
-					<Input
-						label="Phone"
-						placeholder="555-555-5555"
-						value={this.props.phone}
-						onChangeText={value => this.props.employeeUpdate({ prop: 'phone', value })}
-					/>
-				</CardSection>
-
-				<CardSection>
-					<Picker
-						style={{ flex: 1 }}
-						selectedValue={this.props.shift}
-						onValueChange={value => this.props.employeeUpdate({ prop: 'shift', value })}
-
-					>
-						<Picker.Item label="Monday" value="Monday" />
-						<Picker.Item label="Tuesday" value="Tuesday" />
-						<Picker.Item label="Wednesday" value="Wednesday" />
-						<Picker.Item label="Thursday" value="Thursday" />
-						<Picker.Item label="Friday" value="Friday" />
-						<Picker.Item label="Saturday" value="Saturday" />
-						<Picker.Item label="Sunday" value="Sunday" />
-					</Picker>
-				</CardSection>
-
+				<EmployeeForm {...this.props} />
 				<CardSection>
 					<Button onPress={this.onButtonPress.bind(this)}>
 						Create
