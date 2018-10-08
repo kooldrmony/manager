0256d60c67ffd0e78dca8102ba3c25d1d5e11d9a
kooldrmony <derrickreyes.23@gmail.com>
2018-10-07 21:46:02 -0700

    updated confirm component

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import EmployeeForm from './EmployeeForm';
-import { Card, CardSection, Button } from './common';
+import { Card, CardSection, Button, Confirm } from './common';
 import { employeeUpdate, employeeSave } from '../actions';
 import _ from 'lodash';
 import Communications from 'react-native-communications';
 
 class EmployeeEdit extends Component {
+	consturctor()
+	super();
+	this.state = {
+		showModal: false
+	};
+	
 	componentWillMount() {
 		_.each(this.props.employee, (value, prop) => {
 			this.props.employeeUdate({ prop, value });
@@ -40,6 +46,18 @@ class EmployeeEdit extends Component {
 						Text Schedule
 					</Button>
 				</CardSection>
+
+				<CardSection>
+					<Button onPress={() => this.setState({ showModal: !this.state.showModal })}>
+						Fire Employee
+					</Button>
+				</CardSection>
+
+				<Confirm
+					visible={this.state.showModal}
+				>
+					Are you sure you want to delete this employee?
+				</Confirm>
 			</Card>
 
 			)
--- src/components/common/Confirm.js
+++ src/components/common/Confirm.js
@@ -4,6 +4,8 @@ import { CardSection } from './CardSection';
 import { Button } from './Button';
 
 const Confirm = ({ children }) => {
+	const { textStyle, cardSectionStyle, containerStyle } = styles;
+
 	return(
 		<Modal
 			animationType="slide"
@@ -11,9 +13,9 @@ const Confirm = ({ children }) => {
 			transparent
 			visible={visible}
 		>
-			<View>
-				<CardSection>
-					<Text>{children}</Text>
+			<View style={containerStyle}>
+				<CardSection style={cardSectionStyle}>
+					<Text style={textStyle}>{children}</Text>
 				</CardSection>
 
 				<CardSection>
@@ -23,6 +25,25 @@ const Confirm = ({ children }) => {
 			</View>
 		</Modal>
 	)
+
+};
+
+const styles = {
+	cardSectionStyle: {
+		justifyContent: 'center'
+	},
+	textStyle: {
+		flex: 1,
+		fontSize: 18,
+		textAlign: 'center',
+		lineHeight: 40
+	},
+	containerStyle: {
+		backgroundColor: 'rgb(0, 0, 0, 0.75)',
+		position: 'relative',
+		flex: 1,
+		justifyContent: 'center'
+	}
 
 };
 
