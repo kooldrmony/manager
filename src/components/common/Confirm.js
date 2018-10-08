729fcaf3658b856b115b6e152f8a18550518854f
kooldrmony <derrickreyes.23@gmail.com>
2018-10-07 20:59:47 -0700

    added confirm component

---
+++ src/components/common/Confirm.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Text, View, Modal } from 'react-native';
+import { CardSection } from './CardSection';
+import { Button } from './Button';
+
+const Confirm = ({ children }) => {
+	return(
+		<Modal
+			animationType="slide"
+			onRequestClose={() => {}}
+			transparent
+			visible={visible}
+		>
+			<View>
+				<CardSection>
+					<Text>{children}</Text>
+				</CardSection>
+
+				<CardSection>
+					<Button onPress={onAccept}>Yes</Button>
+					<Button onPress={onDecline}>No</Button>
+				</CardSection>
+			</View>
+		</Modal>
+	)
+
+};
+
+export { Confirm };
